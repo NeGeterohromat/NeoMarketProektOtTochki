cf5a53f12010a2a8bd12846457fae77965dc19e2
Saveliy <p89617625637@gmail.com>
2026-05-13 21:56:43 +0500

    feat/add cagetgories crud

--- NeoMarket/b2b/views.py
+++ NeoMarket/b2b/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from app.models import Category
+from .serializers import CategorySerializer, CategoryDetailSerializer
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    http_method_names = ['get', 'post', 'patch', 'delete', 'head', 'options']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        if self.action == 'retrieve':
+            queryset = Category.objects.prefetch_related('subcategories')
+        
+        return queryset
+    
+    def get_serializer_class(self):
+        serializer_class = super().get_serializer_class()
+        
+        if self.action == 'retrieve':
+            serializer_class = CategoryDetailSerializer
+        
+        return serializer_class 
