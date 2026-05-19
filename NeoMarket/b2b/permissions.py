e491684ac818af30557d27c84c8bef4cccb5745d
Saveliy <p89617625637@gmail.com>
2026-05-19 13:59:34 +0500

    fix: has_permission для post

--- NeoMarket/b2b/permissions.py
+++ NeoMarket/b2b/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework import permissions
 
-from app.models import ProductStatus
+from app.models import ProductStatus, Product
 
 
 class CanUpdateProduct(permissions.BasePermission):
@@ -15,6 +15,23 @@ class CanUpdateProduct(permissions.BasePermission):
     
 
 class CanCreateUpdateSKU(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            product_id = request.data.get('product_id')
+            if not product_id:
+                return True # Позволяем сериализатору самому выкинуть ошибку 400 о пустом поле
+            try:
+                product = Product.objects.get(pk=product_id)
+                if request.user != product.seller:
+                    self.message = "Product does not belong to the authenticated seller"
+                    return False
+                if product.status == ProductStatus.HARD_BLOCKED:
+                    self.message = "Cannot edit hard-blocked product"
+                    return False
+            except Product.DoesNotExist:
+                return True # Позволяем сериализатору выкинуть 400 ошибку о несуществующем продукте
+        return True
+    
     def has_object_permission(self, request, view, obj):
         if request.user != obj.product.seller:
             self.message = "Product does not belong to the authenticated seller"
