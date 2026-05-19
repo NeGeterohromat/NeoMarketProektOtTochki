481a4af3ef3221f861870ddfdf6eed92133ad68f
Saveliy <p89617625637@gmail.com>
2026-05-19 14:12:29 +0500

    test: тесты для sku create

--- NeoMarket/b2b/urls.py
+++ NeoMarket/b2b/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('products/', ProductCreateAPIView.as_view(), name='product-create'),
     path('products/<uuid:pk>/', ProductUpdateAPIView.as_view(), name='product-update'),
-    path('skus/create/', SKUCreateAPIView.as_view(), name='create-sku'),
+    path('skus/create/', SKUCreateAPIView.as_view(), name='sku-create'),
     path('skus/<uuid:pk>/', SKUUpdateAPIView.as_view(), name='sku-update'),
 ]
