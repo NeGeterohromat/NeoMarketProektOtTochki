0e65861626831e83a690dd15937013ee083c562f
Saveliy <p89617625637@gmail.com>
2026-05-31 18:03:07 +0500

    fix: формат пагинации для B2CProduct view

--- NeoMarket/b2b/pagination.py
+++ NeoMarket/b2b/pagination.py
@@ -1,6 +1,15 @@
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
 
 
 class B2CProductPagination(LimitOffsetPagination):
     default_limit = 20
     max_limit = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            'items': data,
+            'total_count': self.count,
+            'limit': self.limit,
+            'offset': self.offset,
+        })
